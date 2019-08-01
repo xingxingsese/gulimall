aca1ec2d67e1a8537dff653d8922fc55614bf6aa
xingxingsese <zuihou098@qq.com>
2019-08-01 19:34:10 +0800

    项目初始化

---
+++ gulimall-pom/src/test/java/com/atguigu/gulimall/pom/GulimallPomApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atguigu.gulimall.pom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GulimallPomApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
